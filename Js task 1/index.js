4ab06036eab4226ca79bee91f7997edb5a679753
keibibev <105406071+keibibev@users.noreply.github.com>
2022-06-22 03:53:01 +0100

    Add files via upload

---
+++ Js task 1/index.js
@@ -0,0 +1,8 @@
+let myname="Tamunokeibi Beverly Bristol";
+let myheight="159cm";
+let mycountry="Nigeria";
+
+
+document.getElementById("myname").innerHTML="Name: " + myname;
+document.getElementById("myheight").innerHTML="Height: " + myheight;
+document.getElementById("mycountry").innerHTML="Country: " + mycountry;
